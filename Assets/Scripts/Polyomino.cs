2845a1239de3a6c093611581b9e3e5d3efdef735
chicken-pop <popcopy16youth0723@outlook.jp>
2023-04-23 12:28:47 +0900

    ポリオミノの操作

--- Assets/Scripts/Polyomino.cs
+++ Assets/Scripts/Polyomino.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Polyomino : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 offset;
+
+    //マウスがクリックされた時に実行される処理
+    private void OnMouseDown()
     {
-        
+        offset =  transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    // Update is called once per frame
-    void Update()
+    //マウスがドラックされている間に実行される処理
+    private void OnMouseDrag()
     {
-        
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+        transform.position = new Vector3(newPosition.x , newPosition.y, transform.position.x);
     }
 }
